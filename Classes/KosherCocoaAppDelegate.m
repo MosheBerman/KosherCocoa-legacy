88b909822b3c886eda5598af35929316faf20657
Moshe Berman <moshberm@gmail.com>
2012-11-26 16:19:30 -0500

    Removed demo portion of  project.

--- Classes/KosherCocoaAppDelegate.m
+++ Classes/KosherCocoaAppDelegate.m
@@ -23,7 +23,6 @@
     // Override point for customization after application launch.
 
     // Add the view controller's view to the window and display.
-    [self.window addSubview:viewController.view];
     [self.window makeKeyAndVisible];
 
     return YES;
